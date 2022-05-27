3f87abc143e887875c55cad14fc53ed7965a277b
shangshun <shangshun@codemao.cn>
2022-05-27 19:10:09 +0800

    fix

--- page/component/category/category.js
+++ page/component/category/category.js
@@ -1,12 +1,9 @@
 Page({
     data: {
         category: [
-            {name:'果味',id:'guowei'},
-            {name:'蔬菜',id:'shucai'},
             {name:'炒货',id:'chaohuo'},
             {name:'点心',id:'dianxin'},
             {name:'粗茶',id:'cucha'},
-            {name:'淡饭',id:'danfan'}
         ],
         detail:[],
         curIndex: 0,
